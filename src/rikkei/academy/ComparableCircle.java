f835b6b28ff1f3f69c57eba2d56691c0e5f2613b
minhkhiet <minhkhiet14151993@gmail.com>
2023-03-13 19:11:47 +0700

    111

---
+++ src/rikkei/academy/ComparableCircle.java
@@ -0,0 +1,29 @@
+package rikkei.academy;
+
+import java.util.Comparator;
+
+public class ComparableCircle extends Circle implements Comparable<ComparableCircle> {
+    public ComparableCircle() {
+    }
+
+    public ComparableCircle(double radius) {
+        super(radius);
+    }
+
+    public ComparableCircle(double radius, String color, boolean filled) {
+        super(radius, color, filled);
+    }
+
+    @Override
+    public int compareTo(ComparableCircle o) {
+        if (getRadius() > o.getRadius()) return 1;
+        else if (getRadius() < o.getRadius()) return -1;
+        else return 1;
+    }
+
+
+    @Override
+    public int compare(ComparableCircle o) {
+        return 0;
+    }
+}
